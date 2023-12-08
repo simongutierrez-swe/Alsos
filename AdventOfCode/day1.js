58bfb0e9b5fcc1f73ec7c36f9de1867cd137f922
SimonG <saguti15@g.holycross.edu>
2023-12-08 14:35:10 -0500

    SG: solved part 2

--- AdventOfCode/day1.js
+++ AdventOfCode/day1.js
@@ -27,7 +27,7 @@ Consider your entire calibration document. What is the sum of all of the calibra
 const fs = require('fs')
 
 const input = fs.readFileSync('./input.txt').toString();
-const test = 'two1nine';
+const test = 'two1nine eightwothree abcone2threexyz xtwone3four 4nineeightseven2 zoneight234 7pqrstsixteen';
 
 const numsBank = {
     one: '1',
@@ -41,33 +41,50 @@ const numsBank = {
     nine: '9'
 };
 
+const findWrittenNum = (strLen3, strLen4, strLen5) => {
+    if (numsBank[strLen3]) {
+        return numsBank[strLen3];
+    } else if (numsBank[strLen4]) {
+        return numsBank[strLen4]
+    } else if (numsBank[strLen5]) {
+        return numsBank[strLen5]
+    }
+}
+
 const findCalibrationVals = (str) => {
     let result = str.split(' ');
     let total = 0;
 
-    result.forEach( (e, idx, arr) => {
+    result.forEach( e => {
         let start = 0, end = e.length - 1, first = '', second = '';
 
         while (start < end && !first || !second) {
             if (!first) {
-                const word1 = e + result[idx + 1] + result[idx + 2];
-                const word2 = first + result[idx + 3];
-                const word3 = second + result[idx + 4];
-
-                console.log(arr[idx + 1]);
+                const word1 = e[start] + e[start + 1] + e[start + 2];
+                const word2 = word1 + e[start + 3];
+                const word3 = word2 + e[start + 4];
+                let isWrittenNum = findWrittenNum(word1, word2, word3);
 
                 if (Number(e[start])) {
                     first = e[start];
-                // } else if (isWrittenNum()) {
-
+                } else if (isWrittenNum) {
+                    first = isWrittenNum;
                 } else {
                     start++;
                 }
             }
 
             if (!second) {
+                const word1 = e[end - 2] + e[end - 1] + e[end];
+                const word2 = e[end - 3] + word1;
+                const word3 = e[end - 4] + word2;
+                let isWrittenNum = findWrittenNum(word1, word2, word3);
+                // console.log(word1, word2, word3);
+
                 if (Number(e[end])) {
                     second = e[end];
+                } else if (isWrittenNum) {
+                    second = isWrittenNum;
                 } else {
                     end--;
                 }
@@ -86,7 +103,7 @@ const findCalibrationVals = (str) => {
     return total;
 };
 
-console.log(findCalibrationVals(test)); // 54667
+console.log(findCalibrationVals(input)); // 54667
 
 /*
 --- Part Two ---
@@ -105,15 +122,6 @@ zoneight234
 In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
 */
 
-const isWrittenNum = (strLen3, strLen4, strLen5) => {
-    if (numsBank[strLen3]) {
-        return numsBank[strLen3];
-    } else if (numsBank[strLen4]) {
-        return numsBank[strLen4]
-    } else if (numsBank[strLen5]) {
-        return numsBank[strLen5]
-    }
-}
 
 const isNum = (str) => {
 
